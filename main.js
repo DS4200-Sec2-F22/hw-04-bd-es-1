45992474918ca7fd0b0ece9a35b5de47db4fc884
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-10-06 16:04:43 -0400

    added points to the graph, gave css classes to them, adding functionality

--- main.js
+++ main.js
@@ -2,4 +2,56 @@
 // Brady Duncan and Evan Suslovich
 // Last Modified: 10.5.2022
 
-console.log("Hello world")
+
+
+// starting points
+const xAxis = [1, 2, 6, 9]
+const yAxis = [2, 4, 2, 9]
+
+// get viewbox by id 
+const chart = document.getElementById('viewbox');
+
+
+// scale of chart
+const scale = 5;
+
+
+// placing points 
+for (let i = 0; i < xAxis.length; i++) {
+
+  const svg = document.getElementById('viewbox'); //Get svg element
+  const newElement = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //Create a path in SVG's namespace
+
+  newElement.setAttribute("cx", xAxis[i] * scale);
+  newElement.setAttribute("cy", 50 - (yAxis[i] * scale));
+  newElement.setAttribute("r", 2);
+  newElement.classList.add('point')
+  svg.appendChild(newElement);
+}
+
+
+function pointClicked() {
+  this.classList.toggle("border");
+
+  let xPos = (this.getAttribute("cx") / 5);
+  let yPos = 10 - (this.getAttribute("cy") / 5);
+
+  let clickedPointInfo = "Coordinates: (" + xPos + ", " + yPos + ")";
+
+  document.getElementById("clickedPointInfo").innerHTML = clickedPointInfo;
+
+}
+
+
+const circleList = document.getElementsByTagName("circle");
+
+// adds event listener to each point 
+for (let i = 0; i < circleList.length; i++) {
+  document.getElementsByTagName("circle")[i].addEventListener("click", pointClicked);
+}
+
+
+
+
+
+
